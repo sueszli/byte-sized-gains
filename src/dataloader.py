800352569eaf54d05be159b34bf09c11da251fbf
sueszli <jabaryyahya@gmail.com>
2024-10-16 23:04:10 +0200

    up

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,21 +1,17 @@
-
-from PIL import Image
-from utils import get_device
-from tqdm import tqdm
-from utils import set_env
-import os
 import json
-from torch.utils.data import DataLoader, random_split
-from torchvision import datasets
-import torch
+import os
 from pathlib import Path
-from transformers import AutoImageProcessor, DetrForObjectDetection
+from types import SimpleNamespace
+
+import torch
+from PIL import Image
+from tqdm import tqdm
+from transformers import DetrFeatureExtractor, DetrForObjectDetection
+
 from datasets import load_dataset
+from utils import get_device, set_env
 
 set_env(seed=42)
-
-batch_size = 1
-threshold = 0.8
 
 classes_path = Path.cwd() / "data"
 dataset_path = Path.cwd() / "datasets"
@@ -25,36 +21,46 @@ os.makedirs(classes_path, exist_ok=True)
 os.makedirs(dataset_path, exist_ok=True)
 os.makedirs(weights_path, exist_ok=True)
 
-# 
-# data
-# 
+
+def main(args: dict):
+    device = get_device(disable_mps=False)
+    model_id = "facebook/detr-resnet-101-dc5"  # largest model
+    feature_extractor = DetrFeatureExtractor.from_pretrained(model_id, cache_dir=weights_path, local_files_only=True)  # maybe also threshold arg
+    model = DetrForObjectDetection.from_pretrained(model_id, cache_dir=weights_path, local_files_only=True)
+    model.eval()
+    model.to(device)
+
+    coco_classes = json.load(open(classes_path / "coco_classes.json", "r"))
+    testset = load_dataset("rafaelpadilla/coco2017", split="val", streaming=False, cache_dir=dataset_path)  # can't .map(toTensor()) in memory for batching
+
+    for sample in tqdm(testset):
+        image: Image.Image = sample["image"]
+        image_id: int = sample["image_id"]
+        true_bboxes: dict = sample["objects"]
+
+        image = image.convert("RGB")
+        inputs = feature_extractor(images=image, return_tensors="pt")
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+        with torch.inference_mode():
+            outputs = model(**inputs)
+
+        target_sizes = torch.tensor([image.size[::-1]]).to(device)
+        results = feature_extractor.post_process_object_detection(outputs, args.threshold, target_sizes=target_sizes)
+
+        # outputs = {k: v.cpu() for k, v in outputs.items()}
+        # logits = outputs["logits"]
+        # bboxes = outputs["pred_boxes"]
+        print(len(results))
 
 
 if __name__ == "__main__":
-    testset = load_dataset("rafaelpadilla/coco2017", split="val", streaming=False, cache_dir=dataset_path)
-    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=torch.cuda.is_available())
+    args = SimpleNamespace(
+        threshold=0.5,
+    )
+    main(args)
 
-    for elem in testloader:
-        print(elem.keys())
-
-        exit()
-
-# 
-# model
-# 
 
 # https://ai.google.dev/edge/litert/models/convert_pytorch
-
-# model_id = "facebook/detr-resnet-101-dc5"  # largest model
-# device = get_device(disable_mps=False)
-
-# image_processor = AutoImageProcessor.from_pretrained(model_id, device=device)
-# model = DetrForObjectDetection.from_pretrained(model_id, device=device, local_files_only=True, cache_dir=weights_path, threshold=threshold, return_labels_only=True)
-# model.eval()
-
-
-# inputs = image_processor(images=img, return_tensors="pt")
-# inputs = {k: v.to(device) for k, v in inputs.items()}
 
 # with torch.no_grad():
 #     outputs = model(**inputs)
@@ -66,16 +72,6 @@ if __name__ == "__main__":
 # results["scores"] = [elem.cpu().item() for elem in results["scores"]]
 # results["labels"] = [model.config.id2label[elem.cpu().item()] for elem in results["labels"]]
 
-# # model_labels = [model.config.id2label[elem.item()] for elem in results["labels"]]
-# # results["labels"] = []
-# # for ml in model_labels:
-# #     for cl in labels:
-# #         if cl.lower() in ml.lower():
-# #             results["labels"].append(cl)
-# #             break
-# #     else:
-# #         results["labels"].append("unknown")
-
 # boxes = results["boxes"]
 # scores = results["scores"]
 # labels = results["labels"]
@@ -85,4 +81,3 @@ if __name__ == "__main__":
 # assert all(isinstance(score, float) for score in scores)
 # assert all(isinstance(box, list) and len(box) == 4 for box in boxes)
 # assert all(all(isinstance(coord, float) for coord in box) for box in boxes)
-
