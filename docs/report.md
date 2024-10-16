7aafa0bfd1e97c30bcf37e1094c591831298c5b8
sueszli <jabaryyahya@gmail.com>
2024-10-16 23:34:31 +0200

    up

--- docs/report.md
+++ docs/report.md
@@ -16,7 +16,7 @@ toc: false
 Table of Contents:
 
 - Introduction: Brief about Object detection models (ODMs), LLMs, and their applications
-- Background: The need for quantization, challenges in deploying ODMS and  LLMs, and an overview of quantization techniques
+- Background: The need for quantization, challenges in deploying ODMS and LLMs, and an overview of quantization techniques
 - Experiments: Explained setup and goals of the experiments
 - Results: Detailed results from the quantization, including the benefits and any trade-offs. It should include at least 3 plots:
 - Results 1.1:
@@ -50,7 +50,7 @@ COCO dataset 2017 leaderboard:
 - https://paperswithcode.com/sota/object-detection-on-coco
 - https://github.com/ETH-DISCO/advx-bench/tree/main/analysis/model-selection
 
-DETR family performs the best. "facebook/detr-resnet-101-dc5" in particular seems to also generalize across multiple datasets and be both zero-shot and open vocabulary. (But theoretically we could take any model of our choice).
+DETR family performs the best. "facebook/detr-resnet-101-dc5" in particular seems to also generalize across multiple datasets and be both zero-shot and open vocabulary. (But theoretically we could take any model of our choice). Also this specific DETR model was trained on COCO 2017 dataset which should give it an advantage.
 
 # Results 1.2
 
---
+++ src/det.py
@@ -0,0 +1,100 @@
+import os
+from pathlib import Path
+from types import SimpleNamespace
+
+import torch
+from PIL import Image
+from torch.utils.benchmark import Timer
+from tqdm import tqdm
+from transformers import DetrFeatureExtractor, DetrForObjectDetection
+
+from datasets import load_dataset
+from utils import get_device, set_env
+
+set_env(seed=42)
+
+classes_path = Path.cwd() / "data"
+dataset_path = Path.cwd() / "datasets"
+weights_path = Path.cwd() / "weights"
+
+os.makedirs(classes_path, exist_ok=True)
+os.makedirs(dataset_path, exist_ok=True)
+os.makedirs(weights_path, exist_ok=True)
+
+
+def main(args: dict):
+    device = get_device(disable_mps=False)
+    model_id = "facebook/detr-resnet-101-dc5"
+    feature_extractor = DetrFeatureExtractor.from_pretrained(model_id, cache_dir=weights_path, local_files_only=True)
+    model = DetrForObjectDetection.from_pretrained(model_id, cache_dir=weights_path, local_files_only=True)
+    model.eval()
+    model.to(device)
+
+    testset = load_dataset("rafaelpadilla/coco2017", split="val", streaming=False, cache_dir=dataset_path)  # can't map(toTensor()) in-memory for batching
+
+    for sample in tqdm(testset):
+        metrics = {
+            "accuracy": 0,
+            "precision": 0,
+            "recall": 0,
+            "inference_time": 0,
+        }
+
+        # ground truth
+        image: Image.Image = sample["image"]
+        image_id: int = sample["image_id"]
+        true_bboxes: dict = sample["objects"]
+
+        # preprocessing
+        image = image.convert("RGB")
+        inputs = feature_extractor(images=image, return_tensors="pt")
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+
+        # benchmark inference
+        def inference():
+            with torch.inference_mode():
+                outputs = model(**inputs)
+            return outputs
+
+        timer = Timer(
+            stmt="inference()",
+            globals={"inference": inference, "model": model, "inputs": inputs, "torch": torch},
+        )
+        measurement = timer.blocked_autorange()
+        metrics["inference_time"] = measurement.mean
+
+        # inference
+        with torch.inference_mode():
+            outputs = model(**inputs)
+
+        # postprocessing
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = feature_extractor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=args.threshold)[0]
+        results["boxes"] = [elem.cpu().tolist() for elem in results["boxes"]]
+        results["scores"] = [elem.cpu().item() for elem in results["scores"]]
+        results["labels"] = [elem.cpu().item() for elem in results["labels"]]
+        results["labels_names"] = [model.config.id2label[elem] for elem in results["labels"]]
+        boxes = results["boxes"]
+        scores = results["scores"]
+        labels = results["labels"]
+        label_names = results["labels_names"]
+        assert all(isinstance(box, list) and len(box) == 4 for box in boxes)
+        assert all(isinstance(score, float) for score in scores)
+        assert all(isinstance(label, int) for label in labels)
+        assert all(isinstance(label_name, str) for label_name in label_names)
+        assert all(all(isinstance(coord, float) for coord in box) for box in boxes)
+
+        # # performance metrics
+        # for box, score, label, label_name in zip(boxes, scores, labels, label_names):
+        #     print(f"{label_name}/{label} - {score:.2f} @ {box}")
+
+        print(f"Image {image_id} - Inference time: {metrics['inference_time']:.2f}s")
+
+    # https://ai.google.dev/edge/litert/models/convert_pytorch
+
+
+if __name__ == "__main__":
+    args = SimpleNamespace(
+        threshold=0.9,
+    )
+    main(args)
